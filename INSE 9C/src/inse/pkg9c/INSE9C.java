8f3cdfa7561237a97f6fb229100eb077b817807a
HankyAd <adam.hnkn@gmail.com>
2017-02-14 14:22:48 +0000

    Add files via upload

---
+++ INSE 9C/src/inse/pkg9c/INSE9C.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inse.pkg9c;
+
+/**
+ *
+ * @author up777621
+ */
+public class INSE9C {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
