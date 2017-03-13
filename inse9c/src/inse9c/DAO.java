edc8deb10d36a2868006276f45f0aab9a14e4e73
HankyAd <adam.hnkn@gmail.com>
2017-03-13 17:33:37 +0000

    CONNECTION TO DATABASE WORKS

--- inse9c/src/inse9c/DAO.java
+++ inse9c/src/inse9c/DAO.java
@@ -4,58 +4,63 @@
  * and open the template in the editor.
  */
 package inse9c;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author up777621
  */
 public class DAO {
+
+    private static Statement stmt = null;
     
-    public DAO(){
-        //connect();
+    public DAO() {
     }
-    /*
+
     public static Connection connect(){
-        try {
-        Class.forName("org.postgresql.Driver");
-        String dbName = System.getenv("RDS_DB_NAME");
-        String userName = System.getenv("RDS_USERNAME");
-        String password = System.getenv("RDS_PASSWORD");
-        String hostname = System.getenv("RDS_HOSTNAME");
-        String port = System.getenv("RDS_PORT");
-        String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-        logger.trace("Getting remote connection with connection string from environment variables.");
-        Connection con = DriverManager.getConnection(jdbcUrl);
-        logger.info("Remote connection successful.");
-      return con;
+        Connection conn=null;
+        try{
+            // Load the driver class
+            Class.forName("com.mysql.jdbc.Driver");
+            // Open the connection to the database
+            conn = DriverManager.getConnection("jdbc:mysql://inse9c.cslzqhqslqaw.eu-west-2.rds.amazonaws.com:3306","admin","Inseteam9c");
+            stmt = conn.createStatement();
+        }catch(ClassNotFoundException ce){
+            ce.printStackTrace();
+            System.out.println("Exception: "+ce);
+        }catch(SQLException sqe){
+            sqe.printStackTrace();
+            System.out.println("Exception: "+sqe);
         }
-    catch (ClassNotFoundException e) { logger.warn(e.toString());}
-    catch (SQLException e) { logger.warn(e.toString());}
+        if (conn == null){
+            System.out.println("Connection Failed");
         }
+        return conn;
     }
-    
-    */
-    
-    public static byte[] computeHash(String x) throws Exception{
+
+    public static byte[] computeHash(String x) throws Exception {
         java.security.MessageDigest d = null;
         d = java.security.MessageDigest.getInstance("SHA-1");
         d.reset();
         d.update(x.getBytes());
         return d.digest();
     }
-    
-    public static String byteArraytoHexString(byte[] b){
+
+    public static String byteArraytoHexString(byte[] b) {
         StringBuffer sb = new StringBuffer(b.length * 2);
-        for (int i = 0; i < b.length; i++){
+        for (int i = 0; i < b.length; i++) {
             int v = b[i] & 0xff;
-            if(v < 16)
+            if (v < 16) {
                 sb.append('0');
+            }
             sb.append(Integer.toHexString(v));
         }
         return sb.toString().toUpperCase();
     }
-    
-    
 }
--- inse9c/src/inse9c/Inse9c.java
+++ inse9c/src/inse9c/Inse9c.java
@@ -3,7 +3,6 @@ public class Inse9c {
     public static void main(String[] args) {
         LoginUI l = new LoginUI();
         l.setVisible(true);
-        //DAO dao = new DAO();
     }
     
 }
