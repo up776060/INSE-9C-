0e19c3602cd73f6485934e39720a1b36898a5495
up777621 <up777621@S7D3497F65368D0.uni.ds.port.ac.uk>
2017-03-13 15:11:32 +0000

    attempt at db connection made. will complete later tonight

--- inse9c/src/inse9c/DAO.java
+++ inse9c/src/inse9c/DAO.java
@@ -4,16 +4,39 @@
  * and open the template in the editor.
  */
 package inse9c;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author up777621
  */
 public class DAO {
     
+    public DAO(){
+        //connect();
+    }
+    /*
+    public static Connection connect(){
+        try {
+        Class.forName("org.postgresql.Driver");
+        String dbName = System.getenv("RDS_DB_NAME");
+        String userName = System.getenv("RDS_USERNAME");
+        String password = System.getenv("RDS_PASSWORD");
+        String hostname = System.getenv("RDS_HOSTNAME");
+        String port = System.getenv("RDS_PORT");
+        String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+        logger.trace("Getting remote connection with connection string from environment variables.");
+        Connection con = DriverManager.getConnection(jdbcUrl);
+        logger.info("Remote connection successful.");
+      return con;
+        }
+    catch (ClassNotFoundException e) { logger.warn(e.toString());}
+    catch (SQLException e) { logger.warn(e.toString());}
+        }
+    }
     
-    
-    
+    */
     
     public static byte[] computeHash(String x) throws Exception{
         java.security.MessageDigest d = null;
--- inse9c/src/inse9c/Inse9c.java
+++ inse9c/src/inse9c/Inse9c.java
@@ -3,6 +3,7 @@ public class Inse9c {
     public static void main(String[] args) {
         LoginUI l = new LoginUI();
         l.setVisible(true);
+        //DAO dao = new DAO();
     }
     
 }
